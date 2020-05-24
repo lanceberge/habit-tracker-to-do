961cc5a7f2bd45e72fe9cee9aa953f65824cf196
lanceberge <bergeron.lance6@gmail.com>
2020-05-24 18:52:52 -0400

    changed the operation methods

--- habit_tracker.py
+++ habit_tracker.py
@@ -3,106 +3,125 @@ import csv
 import os
 import sys
 
-task_list = []
+habits_list = []
 to_do_list = []
-days_map = {0: 'M', 1: 'T', 2: 'W', 3: 'Th', 4: 'F', 5: 'S', 6: 'Su'}
+days_map = {0: "M", 1: "T", 2: "W", 3: "Th", 4: "F", 5: "S", 6: "Su"}
 today = days_map[date.today().weekday()]
-tasks_changed = False
 is_running = True
 
-class Task:
 
+class Task:
     def __init__(self, name, days=None):
         """A task with a name, days to be completed, 
         frequency to complete, and time_frame for completion"""
         self.name = name
         self.days = days
 
-
     def __str__(self):
         if self.days == None:
-            return self.name
-        str_days = ','.join([day for day in self.days])
-        return f"{self.name}, {str_days}"
+            return f"{self.name}"
+        else:
+            str_days = ",".join(self.days) if len(self.days) != 7 else "daily"
+            return f"{self.name}; {str_days}"
 
 
-def read_list():
-    """parses the habits.csv file and adds each list to task_list"""
-    tasks_changed = False
+def read_file():
     with open(os.path.join(sys.path[0], "habits.csv"), "r") as csv_habits:
         reader = csv.reader(csv_habits)
         for line in reader:
-            if line[2] == "":
+            if line[1] == "":
                 to_do_list.append(Task(line[0]))
             else:
-                days_list = list(map(int, line[1].split('-')))
-                """splits the stores days value into a list, maps it to the int function,
-                and converts it to a list"""
-                task_list.append(Task(line[0], days_list))
+                habits_list.append(Task(line[0], line[1].split("-")))
 
 
-def tasks_for_day(day=today):
-    """returns a list of the string output of each task
-    for parameter day"""
-    return [str(task) for task in task_list if day in task.days]
+def tasks_for_today():
+    [print(task.name) for task in habits_list if today in task.days]
 
 
-def add_task(task):
-    # TODO - Add task to csv
-    task_list.append(task)
-    tasks_changed = True
+def tasks_for_day():
+    day = input("Enter M, T, W, Th, F, S, or Su: ")
+    [print(task.name) for task in habits_list if day in task.days]
 
-def remove_task(task):
-    task_list.remove(task)
-    tasks_changed = True
 
-def task_for_today():
-    print (tasks_for_day())
+def view_all_habits():
+    [print(str(habit)) for habit in habits_list]
 
-def task_for_input_day():
-    day = input(
-    """Enter the day you would like to view tasks for:
-    M, T, W, Th, F, S, or Su
-    """)
-    print (tasks_for_day(day))
 
 def add_task():
-    pass
+    # TODO - Add to CSV
+    name = input("Enter the name of the task: ")
+    habit_or_todo = input("Is this task a habit or a to-do item: ").lower()
+    if habit_or_todo == "habit":
+        days = input(
+            "Enter days to complete this task (M, T, W, Th, F, S, Su), separated by dashes: "
+        )
+        habits_list.append(Task(name, days.split("-")))
+    else:
+        to_do_list.append(Task(name))
+    print("Task added!")
+
 
 def delete_task():
-    pass
+    # TODO - Delete from CSV
+    task_removed = False
+    task_name = input("Enter the name of the task: ")
+    habit_or_todo = input("Is this task a habit or a to-do item: ").lower()
+    task_dic = {"to-do item": to_do_list, "to-do": to_do_list, "habit": habits_list}
+    for task in task_dic[habit_or_todo]:
+        if task.name == task_name:
+            task_dic[habit_or_todo].remove(task)
+            task_removed = True
+            break
+    to_print = "Task removed!" if task_removed else "No task found"
+    print(to_print)
 
-def view_all_tasks():
-    print()
 
-def to_do():
-    print ([str(task) for task in to_do_list])
+def view_todo():
+    print([str(todo) for todo in to_do_list])
+
+
+def view_all():
+    print("Habits: ")
+    view_all_habits()
+    print("To-Do: ")
+    view_todo()
+
 
 def close():
+    global is_running
     is_running = False
 
+
 def main():
+    read_file()
     operations = {
-        1: task_for_today,
-        2: task_for_input_day,
-        3: add_task,
-        4: delete_task,
-        5: view_all_tasks,
-        6: to_do,
-        7: close 
+        "1": tasks_for_today,
+        "2": tasks_for_day,
+        "3": view_all_habits,
+        "4": add_task,
+        "5": delete_task,
+        "6": view_todo,
+        "7": view_all,
+        "8": close,
     }
     while is_running:
         i = input(
-        """What would you like to do?
+            """What would you like to do?
         1. View habits for Today
         2. View habits for day
-        3. Add a task/habit
-        4. Delete a task/habit
-        5. View to-do list
-        6. Close
+        3. View all habits
+        4. Add a task/habit
+        5. Delete a task/habit
+        6. View to-do list
+        7. View all tasks and habits
+        8. Close
         Only enter the number: """
         )
-        operations[i]()
+        try:
+            operations[i]()
+        except:
+            print("operation not supported")
 
 
 if __name__ == "__main__":
